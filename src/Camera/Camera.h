39ac9432d59b45f2dfe96a9d36a43e3b68ba7c07
Владимир Шаталов <gamedevshttl@gmail.com>
2023-05-08 15:38:36 +0300

    increase camera speed

--- src/Camera/Camera.h
+++ src/Camera/Camera.h
@@ -18,7 +18,7 @@ private:
 	glm::vec3 position{ glm::vec3(0.0f, 0.0f, 7.0f) };
 	glm::vec3 front{ glm::vec3(0.0f, 0.0f, -1.0f) };
 	glm::vec3 up{ glm::vec3(0.0f, 1.0f, 0.0f) };
-	GLfloat speed{ 0.5f };
+	GLfloat speed{ 1.0f };
 
 	bool pressed[GLFW_KEY_LAST] = {};
 	GLfloat lastX, lastY;
